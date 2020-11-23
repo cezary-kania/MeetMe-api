e02f10d1c1f17801db9941c83c0b4912e58d8d4b
cezary-kania <cezary.kaniadev@gmail.com>
2020-11-24 00:02:34 +0100

    add postgresql, mongodb, configure docker-compose

--- src/app/Data/UserDatabase.py
+++ src/app/Data/UserDatabase.py
@@ -2,12 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "" # production database
-SQLALCHEMY_DATABASE_URL = "sqlite:///../app.db" # development database
+import os
 
-
+SQLALCHEMY_DATABASE_URL = os.getenv('DB_URL')
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
